6c6cecfa0d73bfadfbc4d1390fbe50fba502485b
Bhavya Jain <jainbhavya2306@gmail.com>
2025-04-18 22:15:23 +0530

    Time: 2 ms (92.38%), Space: 41.3 MB (79.02%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String countAndSay(int n) {
+        String s = "1";
+        for (int i = 2; i <= n; i++) {
+            s = helper(s);
+        }
+        return s;
+    }
+
+    private String helper(String s) {
+        StringBuilder sb = new StringBuilder();
+        char prev = s.charAt(0);
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == prev) {
+                count++;
+            } else {
+                sb.append(count).append(prev);
+                prev = s.charAt(i);
+                count = 1;
+            }
+        }
+        sb.append(count).append(prev);
+        return sb.toString();
+    }
+}
