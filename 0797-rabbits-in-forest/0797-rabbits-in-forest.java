699125c4d59577eaaae030cc9f3e29dc16dcfb86
Bhavya Jain <jainbhavya2306@gmail.com>
2025-04-20 10:09:28 +0530

    Time: 1 ms (93.98%), Space: 42.2 MB (70.99%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.java
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.java
@@ -1,11 +1,13 @@
 class Solution {
     public int numRabbits(int[] answers) {
-        Map<Integer, Integer> fmap = new HashMap<>();
+        int[] freq = new int[1001];
         int count = 0;
-        for(int i : answers) fmap.put(i, fmap.getOrDefault(i, 0) + 1);
-        for(Map.Entry<Integer,Integer> i : fmap.entrySet()) {
-            count += (i.getValue() / (i.getKey() + 1)) * (i.getKey() + 1);
-            if(i.getValue() % (i.getKey() + 1) != 0)  count += (i.getKey() + 1);
+        for (int a : answers)
+            freq[a]++;
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] != 0) {
+                count += (int) Math.ceil((float) freq[i] / (i + 1)) * (i + 1);
+            }
         }
         return count;
     }
